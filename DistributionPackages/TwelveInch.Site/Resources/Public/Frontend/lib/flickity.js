e6ebadca042de68770e3a9a1a8dd2123b7d164c0
Rasmus Andersen <rasmus@growupstudio.dk>
2020-07-10 14:14:15 +0200

    Work in progress: product image carousel

---
+++ DistributionPackages/TwelveInch.Site/Resources/Public/Frontend/lib/flickity.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Initialize Flickity carousel
+
+var element = document.getElementById('original');
+if (typeof element != 'undefined' && element != null) {
+
+	document.addEventListener('DOMContentLoaded', function () {
+		var elem = document.querySelector('.product-carousel');
+		var flkty = new Flickity(elem, {
+			// options
+			cellAlign: 'left',
+			contain: true,
+			pageDots: false,
+			imagesLoaded: true,
+			freeScroll: true,
+			selectedAttraction: 0.08,
+			friction: 0.9
+		});
+	});
+}
